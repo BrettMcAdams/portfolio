2c311a6610b9a0b53330b7aeda13db950583d434
unknown <brettmcadams96@gmail.com>
2020-10-04 04:08:12 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+
 
 function App() {
+
+  const [react, setReact] = useState(false)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="navbar">
+        <a href="#" className="logo">
+          <a>B</a>
         </a>
-      </header>
+        <ul className="nav">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#portfolio">Projects</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div className="banner-area" id="home">
+        <div className="header-text">
+          <h3>Hi, my name is</h3>
+          <h2>Brett McAdams.</h2>
+          <h2>Full Stack Web Engineer.</h2>
+          <p>
+            I'm a software engineer based in Orlando, Fl specializing in
+            creating and designing exceptional, high-quility websites and
+            applications.
+          </p>
+          <div class="arrow bounce">
+            <a href="#about"><ArrowDownwardIcon style={{ fontSize: 60 }} /></a>
+          </div>
+        </div>
+      </div>
+      <div className="about-area" id="about">
+        <div className="about-text">
+          <h2>A little bit about me</h2>
+          <p>
+            Hello! My name is Brett and I'm a software engineer who specializes
+            in front end development. I attended Lambda School in 2020 and
+            finished their Full Stack Web Development program. I aspire to work
+            for a company that will allow me to channel my appreciation for
+            beautiful and creative UI's that create amazing and engaging user
+            experiences.
+          </p>
+          <div className="links">
+            <a>
+              <GitHubIcon style={{fontSize: 40}}/>
+            </a>
+            <a>
+              <TwitterIcon style={{fontSize: 40}} />
+            </a>
+            <a>
+              <LinkedInIcon style={{fontSize: 40}} />
+            </a>
+            <a>
+              <InstagramIcon style={{fontSize: 40}} />
+            </a>
+          </div>
+        </div>
+        <section>
+          <div>
+            <h2>
+              HTML5, CSS, Javascript, React/Redux, Node JS/Express, Python,
+              Firebase, HTML5, CSS, Javascript, React/Redux, Node JS/Express,
+              Python, Firebase, HTML5, CSS, Javascript, React/Redux, Node
+              JS/Express, Python, Firebase
+            </h2>
+          </div>
+          <div>
+            <h2>
+              HTML5, CSS, Javascript, React/Redux, Node JS/Express, Python,
+              Firebase, HTML5, CSS, Javascript, React/Redux, Node JS/Express,
+              Python, Firebase, HTML5, CSS, Javascript, React/Redux, Node
+              JS/Express, Python, Firebase
+            </h2>
+          </div>
+        </section>
+      </div>
+      <div className="port-area" id="portfolio"></div>  
+      <div className="service-area" id="services"></div>
+      <div className="contact-area" id="contact"></div>
     </div>
   );
 }
